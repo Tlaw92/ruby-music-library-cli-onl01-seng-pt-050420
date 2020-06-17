8a95929c589bab64c551c61f63e8fa5c91dd4740
travis Lawson <travislawclimb@gmail.com>
2020-06-17 00:19:41 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,62 +1,75 @@
-class Song 
-  
-  extend Concerns::Creatable
-  
-  @@all = []
-  
-  attr_accessor :name  
-  
-  def initialize(name, artist=nil, genre=nil)
-    if artist
-      raise "artist argument must be instance of artist class" unless artist.class == Artist
-      self.artist = artist
-    end   
-    if genre
-      raise "Genre object must be instance of genre class" unless genre.class == Genre
-      self.genre = genre
-    end   
-    @name = name 
-    save
-    self
-  end   
+class Song
+  attr_accessor :name, :artist, :genre
 
-  def self.all 
-    @@all 
-  end  
-  
-  def self.all=(array)
-    @@all = array  
-  end 
-  
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist=artist if artist
+    # self.artist.songs.push(self)
+    self.genre=genre if genre
+    # @artist = artist
+  end
+
+  def self.all
+    @@all
+  end
+
   def self.destroy_all
-    self.all = []
-  end  
-  
+    @@all.clear
+  end
+
   def save
-    Song.all << self 
-  end 
-  
+    @@all << self
+  end
+
+  def self.create(name)
+    song = self.new(name)
+    song.save
+    song
+  end
+
   def artist=(artist)
     @artist = artist
-    artist.add_song(self) unless artist.songs.index(self)
-  end 
-  
-  def artist
-    @artist
-  end   
-  
-  def genre=(genre)
-    @genre = genre 
-    genre.songs << self unless genre.songs.index(self)
-  end 
-  
-  def genre
-    @genre
-  end 
-  
+    artist.add_song(self)
+    # Artist.all.push(artist)
+  end
 
-    
-    
-  
-   
+  def genre=(genre)
+    @genre = genre
+    if !(genre.songs.include?(self))
+      genre.songs << self
+      # Genre.all.push(genre)
+    # binding.pry
+    end
+  end
+
+  def self.find_by_name(name)
+    all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    # if self.find_by_name(name)
+    #   self.find_by_name(name)
+    # else
+    # song = self.create(name)
+    # end
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
+    artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
+
+    # song = self.find_or_create_by_name(name)
+    genre = Genre.find_or_create_by_name(genre)
+    artist = Artist.find_or_create_by_name(artist)
+
+    new(name,artist,genre)
+  end
+
+  def self.create_from_filename(filename)
+    new_from_filename(filename).tap{ |s| s.save}
+
+  end
 end 
